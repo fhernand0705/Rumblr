44fb607e5c2df8b9f8c126d1d6db216d341d33d6
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-03-19 14:21:19 -0400

    still working on js behavior...adjusted styling to make dashboard page responsive

--- public/main.js
+++ public/main.js
@@ -1,13 +1,16 @@
 // active nav item
-window.addEventListener("load", function(event) {
-  var dash = document.getElementsByClassName('dash')[0];
-  var user = document.getElementsByClassName('user')[0];
-  var post = document.getElementsByClassName('post')[0];
-
-  if(window.location.pathname == '/dashboard') {
-    dash.style.backgroundColor = 'blue';
- } else if (window.location.pathname == "/users/<%= session['user'] %>") {
-    user.style.backgroundColor = 'blue';
- } else if (window.location.pathname == '/posts/create') {
-    post.style.backgroundColor = 'blue';
-}});
+// window.addEventListener("load", function(event) {
+//   var dash = document.getElementsByClassName('dash')[0];
+//   //var user = document.getElementsByClassName('user')[0];
+//
+//   if(window.location.pathname == '/dashboard') {
+//     dash.style.backgroundColor = 'blue';
+//  }});
+//
+//  window.addEventListener("load", function(event) {
+//    var post = document.getElementsByClassName('post')[0];
+//
+//    if(window.location.pathname == '/posts/create') {
+//      post.style.backgroundColor = 'blue';
+//    }
+//  });
